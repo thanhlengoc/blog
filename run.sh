01d128b8f64c0485a1e661811f7e30fb404cb90a
thanhln3 <thanhln3@vng.com.vn>
2020-11-06 17:14:54 +0700

    edit package.json

--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@ git add out/
 git commit -m "Deploy to gh-pages"
 git push origin --delete gh-pages
 git subtree push --prefix out origin gh-pages
+
+"deploy": "next build && next export && git add out/ && git commit -m \"Deploy gh-pages\" && git subtree push --prefix out origin gh-pages"
