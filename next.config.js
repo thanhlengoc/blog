a9c2d843f9b1a9b5868a221415e947def0f09c56
thanhln3 <thanhln3@vng.com.vn>
2020-11-19 14:48:18 +0700

    fix fs

--- next.config.js
+++ next.config.js
@@ -23,16 +23,16 @@ module.exports = withPlugins([
             }
         }
     },
-    {
-        webpack: (config, { isServer }) => {
-            // Fixes npm packages that depend on `fs` module
-            if (!isServer) {
-                config.node = {
-                    fs: 'empty'
-                }
-            }
-
-            return config
-        }
-    }
+    // {
+    //     webpack: (config, { isServer }) => {
+    //         // Fixes npm packages that depend on `fs` module
+    //         if (!isServer) {
+    //             config.node = {
+    //                 fs: 'empty'
+    //             }
+    //         }
+    //
+    //         return config
+    //     }
+    // }
 ]);
